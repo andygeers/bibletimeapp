574fc7a85ded1ec9071339ad98e816158f66a29b
Andy Geers <andy.geers@googlemail.com>
2018-12-14 21:56:15 +0000

    Get a basic time slider working

--- src/index.js
+++ src/index.js
@@ -4,8 +4,59 @@ import ReactDOM from "react-dom";
 import 'bootstrap';
 import './styles/index.scss';
 
-const Index = () => {
-  return <div className="container-fluid">Hello React!</div>;
-};
+var Dragdealer = require('dragdealer');
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+var $ = require('jquery');
+
+class BibleTime extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    new Dragdealer('just-a-slider', {
+      animationCallback: function(x, y) {
+        // 1 = 4 hours
+        var minutes = Math.floor(x * (4 * 60 - 1)) + 1;
+        var hours = Math.floor(minutes / 60);
+        var minsRemainder = minutes - hours * 60;
+
+        var minsLabel;
+        if (minsRemainder == 1) {
+          minsLabel = minsRemainder + ' minute';
+        } else {
+          minsLabel = minsRemainder + ' minutes';
+        }
+
+        var label = "";
+        if (hours > 1) {
+          label = hours + " hours " + minsLabel;
+        } else if (hours == 1) {
+          label = hours + " hour " + minsLabel;
+        } else {
+          label = minsLabel;
+        }
+
+        $('#timeLabel').text(label);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        How long have you got?
+
+        <div id="just-a-slider" className="dragdealer">
+          <div className="handle red-bar">
+
+          </div>
+        </div>
+
+        <div id="timeLabel"></div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<BibleTime />, document.getElementById("index"));
